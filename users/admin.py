12aa7ed1db52d5980729fefde4c41da86388ba03
Ahmed Gamal <agsalamh@gmail.com>
2024-11-19 04:10:51 +0200

    feat: create custom user admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
+from users.models import User
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = ('username', 'role')
+    search_fields = ('username', 'role')
+
+    list_filter = ('role',)
+
+    fieldsets = (
+        (None, {'fields': ('username', 'name', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'role', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'created_at')}),
+    )
+
+    # Fields displayed in the add user view
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'name', 'role', 'password1', 'password2', 'is_active', 'is_staff', 'is_superuser'),
+        }),
+    )
+
+    ordering = ('username',)
+    
+    readonly_fields = ('created_at', 'last_login')
