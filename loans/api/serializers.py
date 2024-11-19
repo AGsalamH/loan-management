dc95bf238ef05246e24a5505a545db51629da3ab
Ahmed Gamal <agsalamh@gmail.com>
2024-11-19 07:45:27 +0200

    feat: create endpoints for handling loans

--- loans/api/serializers.py
+++ loans/api/serializers.py
@@ -1,1 +1,12 @@
 from rest_framework import serializers
+
+from loans.models import Loan
+
+
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Loan
+        fields = ('id', 'name', 'description', 'amount', 
+                  'interest_rate', 'max_loan_amount', 'min_loan_amount',
+                  'duration', 'requested_by', 'assigned_to', 'status'
+                )
--- loans/api/views.py
+++ loans/api/views.py
@@ -1,3 +1,20 @@
 '''
 Views for the loans API.
 '''
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from loans.models import Loan
+from loans.api.serializers import LoanSerializer
+
+from users.api.permissions import IsLoanCustomer
+
+
+
+class LoanViewSet(viewsets.ModelViewSet):
+    '''Create, Retrieve and modify information for a user.'''
+    serializer_class = LoanSerializer
+    permission_classes = (IsAuthenticated, IsLoanCustomer)
+
+    def get_queryset(self):
+        return Loan.objects.all().select_related(
+        'requested_by', 'assigned_to').order_by('-created_at')
